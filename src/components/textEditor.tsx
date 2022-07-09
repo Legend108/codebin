ae08fdbe9377a8b2e3d02c5163078e5058aa345b
Legend108 <80154701+Legend108@users.noreply.github.com>
2022-07-09 18:50:41 +0530

    Add files via upload

--- src/components/textEditor.tsx
+++ src/components/textEditor.tsx
@@ -1,1 +1,25 @@
+import brace from "brace";
+import "brace/mode/javascript";
+import "brace/mode/c_cpp";
+import "brace/theme/twilight";
+import "brace/theme/xcode";
+import AceEditor from "react-ace";
 
+const textEditor = (props: any) => (
+  <div>
+    <AceEditor
+      mode={props.lan}
+      theme={props.theme}
+      onChange={props.onChange}
+      name="editor"
+      editorProps={{
+        $blockScrolling: true,
+      }}
+      fontSize={21}
+      height="80vh"
+      width="100%"
+    />
+  </div>
+);
+
+export default textEditor;
