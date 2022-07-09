1c37afcfe5472569a787d2010a1b8e050e7916c1
Legend108 <80154701+Legend108@users.noreply.github.com>
2022-07-09 18:49:11 +0530

    Add files via upload

--- src/pages/api/createSnippet.ts
+++ src/pages/api/createSnippet.ts
@@ -1,1 +1,13 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import axios from "axios";
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.method == "POST") {
+    const body = req.body;
 
+    res.json({
+        status: "done"
+    });
+  }
+};
+
+export default handler;
